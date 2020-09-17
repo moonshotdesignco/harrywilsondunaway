0023e622225a198de04ed5f2d30d9a45c0e93b3f
harry <moonshotdesignco@gmail.com>
2020-09-17 11:16:00 -0400

    colors

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 document.addEventListener("touchstart", function() {}, false);
+
+$(".bin").fadeTo(1200, 1);
+
+// $(".next").on("click", function() {
+//   $(this).parents(".bin").fadeOut(300, function() {
+//     $(this).next(".bin").fadeTo(700, 1);
+//   });
+// });
+
+// $(".prev").on("click", function() {
+//   $(this).parents(".bin").fadeOut(300, function() {
+//     $(this).prev(".bin").fadeTo(700, 1);
+//   });
+// });
+
+// $(".home").on("click", function() {
+//   $(this).parents(".bin").fadeOut(300, function() {
+//     $("#bin-a").fadeTo(1200, 1);
+//   });
+// });
