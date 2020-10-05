1a1d7576c718f2b2430e85c7c4d40c57ea6b9f65
harry <moonshotdesignco@gmail.com>
2020-10-05 14:43:15 -0400

    styling

--- script.js
+++ script.js
@@ -2,31 +2,13 @@ document.addEventListener("touchstart", function() {}, false);
 
 $(".bin").fadeTo(600, 1);
 
-// $(".next").on("click", function() {
-//   $(this).parents(".bin").fadeOut(300, function() {
-//     $(this).next(".bin").fadeTo(700, 1);
-//   });
-// });
-
-// $(".prev").on("click", function() {
-//   $(this).parents(".bin").fadeOut(300, function() {
-//     $(this).prev(".bin").fadeTo(700, 1);
-//   });
-// });
-
-// $(".home").on("click", function() {
-//   $(this).parents(".bin").fadeOut(300, function() {
-//     $("#bin-a").fadeTo(1200, 1);
-//   });
-// });
-
 $(document).on("scroll", function() {
   var y = $(this).scrollTop();
   var h = $(".bin").height();
-  if (y > h) {
-    $(".bar").fadeIn();
+  if (y > (h / 2)) {
+    $(".scrlbtn").fadeIn(300);
   } else {
-    $(".bar").fadeOut();
+    $(".scrlbtn").fadeOut(500);
   }
 });
 
