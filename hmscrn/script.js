194a7d6c03da59c3e99cc9f76d4f154afbbac100
harry <moonshotdesignco@gmail.com>
2020-10-12 12:32:36 -0400

    calendar

--- hmscrn/script.js
+++ hmscrn/script.js
@@ -1,8 +1,8 @@
 document.getElementById("style-switch-tog").onclick = function() {
     if (document.getElementById("style-switch-tog").checked == true) {
-        document.body.className = "light";
-    } else if (document.getElementById("style-switch-tog").checked == false) {
         document.body.className = "dark";
+    } else if (document.getElementById("style-switch-tog").checked == false) {
+        document.body.className = "light";
     } else {
         return false;
     }
@@ -15,11 +15,35 @@ function time() {
     let mn = date.getMinutes();
     let sc = date.getSeconds();
     
-    hr = (hr < 10) ? `0${hr}` : hr;
-    mn = (mn < 10) ? `0${mn}` : mn;
-    sc = (sc < 10) ? `0${sc}` : sc;
+    var meridian;
     
-    document.getElementById("clock").innerHTML = "<h1>" + `${hr}:${mn}:${sc}` + "</h1>";
+    if (hr >= 12) {
+        meridian = "pm"
+    } else {
+        meridian = "am"
+    }
+    
+    if (hr == 0) {
+        hr = 12;
+    }
+
+    if (hr > 12) {
+        hr = hr - 12;
+    }
+
+    if (hr < 10) {
+        hr = "0" + hr;
+    }
+
+    if (mn < 10) {
+        mn = "0" + mn;
+    }
+
+    if (sc < 10) {
+        sc = "0" + sc;
+    }
+    
+    document.getElementById("clock").innerHTML = "<h1>" + `${hr}:${mn}:${sc} ${meridian}` + "</h1>";
 
     setTimeout(time, 1000);
 
@@ -39,5 +63,17 @@ function today() {
 
     document.getElementById("date").innerHTML = "<h1>" + `${dy}` + ", <br>" + `${dt} ${mo} ${yr}` + "</h1>";
 
-}
+    let calEl = document.getElementById("cal");
+    let tds = calEl.getElementsByTagName("td");
 
+    for (let index = 0; index < tds.length; index++) {
+        let tdsDy = tds[index];
+        let tdToday = tdsDy.innerText;
+        if (tdToday == dt) {
+            tds[index].className = "today";
+        }
+    }
+
+    setTimeout(today, 1000);
+
+}
